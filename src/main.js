a0a9f23e7813496ad1d255bca032c9a17db374b0
xilily <xilily@sina.cn>
2019-06-01 08:06:40 +0800

    暂存release上的修改

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 //检查remote的更改
+//测试rebase的作用
 render (
     <div>sorry,you don't have the access rights</div>,
     document.getElementById('app')
