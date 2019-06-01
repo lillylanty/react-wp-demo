0325983698cd3b5156e05ebaf774154bcfad0685
xilily <xilily@sina.cn>
2019-06-01 08:04:59 +0800

    修改normal.js为main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-
+//检查remote的更改
 render (
     <div>sorry,you don't have the access rights</div>,
     document.getElementById('app')
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const role = NODE_ENV;
-var html = role = 'admin'?  '../src/index.js':'../src/normal.js'
+var html = role = 'admin'?  '../src/index.js':'../src/main.js'
 module.exports = {
     entry: path.resolve(__dirname, html), //指定入口文件，程序从这里开始编译,__dirname当前所在目录, ../表示上一级目录, ./同级目录
     output: {
