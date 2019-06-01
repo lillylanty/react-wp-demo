0e6853a2e560c89fd5b3401663fab254185cc184
xilily <xilily@sina.cn>
2019-06-01 08:30:10 +0800

    change main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 //检查remote的更改
-//测试rebase的作用
+//测试rebase的作用 rebase是需要
 render (
     <div>sorry,you don't have the access rights</div>,
     document.getElementById('app')
